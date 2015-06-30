a58045c713c544ee72adad9d8be7192ba132a1ec
subhrajyotibasu <subhrajyoti.basu@indusnet.co.in>
2015-06-30 15:31:56 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,8 @@
 <?php
 $reg_email ='abc@yahoo.com';
-
 //$domain = substr(strrchr($reg_email, "@"),0);
-
 $domianarr = explode("@",$reg_email);
 print_r($domianarr);
-
 echo $domianname = $domianarr[0];
 $organization = $domianname.'_org';
 ?>
